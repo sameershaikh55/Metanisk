7bf21f134f6e6a3d899f3f7cc8778b013bfdb485
sameer-shaikh <sameermubeen4@gmail.com>
2021-05-16 17:28:14 +0500

    bottom navigation seted

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { AiOutlineHome } from "react-icons/ai";
-import { VscKey } from "react-icons/vsc";
-import { IoConstructOutline } from "react-icons/io5";
-import { BsSearch } from "react-icons/bs";
+import { IoLocationOutline } from "react-icons/io5";
+import { AiOutlineStar, AiOutlineUser } from "react-icons/ai";
 import { NavLink } from "react-router-dom";
 import { GoPlus } from "react-icons/go";
+import { CgHome } from "react-icons/cg";
 
 const BottomNav = () => {
 	return (
@@ -14,26 +13,26 @@ const BottomNav = () => {
 					<div className="main_div">
 						<NavLink exact activeClassName="active" to="/">
 							<div className="each_navigation">
-								<AiOutlineHome className="NavIcon" />
-								<h6>Home</h6>
+								<CgHome className="NavIcon" />
+								Accueil
 							</div>
 						</NavLink>
-						<NavLink exact activeClassName="active" to="/property">
+						<NavLink exact activeClassName="active" to="/searchMap">
 							<div className="each_navigation">
-								<VscKey className="NavIcon" />
-								<h6>Property</h6>
+								<IoLocationOutline className="NavIcon" />
+								Carte
 							</div>
 						</NavLink>
-						<NavLink exact activeClassName="active" to="/">
+						<NavLink exact activeClassName="active" to="/favoris">
 							<div className="each_navigation">
-								<IoConstructOutline className="NavIcon" />
-								<h6>Search</h6>
+								<AiOutlineStar className="NavIcon" />
+								Favoris
 							</div>
 						</NavLink>
-						<NavLink exact activeClassName="active" to="/filter/buy">
+						<NavLink exact activeClassName="active" to="/menuProfile">
 							<div className="each_navigation">
-								<BsSearch className="NavIcon" />
-								<h6>Search</h6>
+								<AiOutlineUser className="NavIcon" />
+								Profile
 							</div>
 						</NavLink>
 					</div>
